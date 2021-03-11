f14b16dde5de9bb55e00c21478ff75f7a316aa35
Mykiel-Horn <mykiel.d.horn24@gmail.com>
2021-03-11 15:28:14 -0600

    Collegiate thesaurus api key can send fetch data, next is Intermediate thesaurus api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 
+const url = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=${process.env.REACT_APP_COLLEGIATE_THESAURUS_API_KEY}`;
+
 function App() {
+	// Use the url as normal in your request
+	fetch(url)
+		.then((res) => res.json())
+		.then(console.log)
+		.catch(console.error);
 	return (
 		<div className='App'>
 			<header className='App-header'>Welcome to Enrich-Vocab!</header>
